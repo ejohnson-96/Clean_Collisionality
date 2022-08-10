afd5e6f7f16bcb2f88bba571384957c69a088561
ejohnson-96 <ejohn@udel.edu>
2022-08-10 15:14:26 -0400

    update

--- modules/collisions/main.py
+++ modules/collisions/main.py
@@ -33,11 +33,13 @@ h = 1
 user_enc = 'EA'
 user_rad = 1.0
 
-path = paths[user_enc][user_rad]
+path1 = paths[user_enc][0.1]
+path2 = paths['E6'][0.1]
+path3 = paths[user_enc][1.0]
 
-theta_i = np.loadtxt(sm.jwos(path,'theta_i.txt'))
-theta_f = np.loadtxt(sm.jwos(path,'theta_f.txt'))
-theta_w = np.loadtxt(sm.jwos(path,'wind_theta.txt'))
+theta_i = np.loadtxt(sm.jwos(path3,'theta_f.txt'))
+theta_f = np.loadtxt(sm.jwos(path2,'wind_theta.txt'))
+theta_w = np.loadtxt(sm.jwos(path3,'theta_f.txt'))
 
 data = [theta_i, theta_f, theta_w]
 
@@ -64,17 +66,20 @@ def fit_function(x, r, m, s, q, n, t, ):
 def second_fit_function(x, r, m, s, q, n, t, ):
     return maxwellian(x, r, m,s) + maxwellian(x,q,n,t)
 
-val = 0.1 #0.000001
-val2 = 0.1 #0.000001
-val3 = 0.1
+val = 0.48
+val2 = val
+val3 = val
 
-bn_i = int((max(theta_i) - min(theta_i))/0.1)
-bn_f = int((max(theta_f) - min(theta_f))/0.1)
-bn_w = 75 #int((max(theta_w)-min(theta_w))/val3)
+n = 1
+
+bn_i = 100 #int((max(theta_i) - min(theta_i))/val)
+bn_f = bn_i #int((max(theta_f) - min(theta_f))/val2)
+bn_w = bn_i #int((max(theta_w)-min(theta_w))/val3)
 
 theta_i = theta_i[np.logical_not(np.isnan(theta_i))]
 theta_f = theta_f[np.logical_not(np.isnan(theta_f))]
-#theta_w = theta_w[np.logical_not(np.isnan(theta_w))]
+theta_w = theta_w[np.logical_not(np.isnan(theta_w))]
+
 
 print(bn_i, bn_f, bn_w)
 
@@ -83,13 +88,23 @@ for i in range(len(theta_i)):
     if not isinstance(theta_i[i], (float, int)):
         print(theta_i[i])
 
-data_entries_i, bins_i = np.histogram(smoothing.smooth(theta_i,1), bins=bn_i, density=True)
-data_entries_f, bins_f = np.histogram(smoothing.smooth(theta_f, 1), bins=bn_f, density=True, )
-#data_entries_w, bins_w = np.histogram(theta_w, bins=bn_w, density=True)
+smooth_val = 1
+
+weg1 = np.ones_like(theta_i)/float(len(theta_i))
+weg2 = np.ones_like(theta_f)/float(len(theta_f))
+weg3 = np.ones_like(theta_w)/float(len(theta_w))
+
+data_entries_i, bins_i= np.histogram(smoothing.smooth(theta_i, smooth_val), range=(0,15), weights=weg1, bins=bn_i, density=1)
+data_entries_f, bins_f = np.histogram(smoothing.smooth(theta_f,smooth_val), range=(0,15), weights=weg2, bins=bn_f, density=1)
+data_entries_w, bins_w = np.histogram(smoothing.smooth(theta_w, smooth_val), range=(0,15), weights=weg3, bins=bn_w, density=1)
 
 binscenters_i = np.array([0.5 * (bins_i[i] + bins_i[i + 1]) for i in range(len(bins_i) - 1)])
 binscenters_f = np.array([0.5 * (bins_f[i] + bins_f[i + 1]) for i in range(len(bins_f) - 1)])
-#binscenters_w = np.array([0.5 * (bins_w[i] + bins_w[i + 1]) for i in range(len(bins_w) - 1)])
+binscenters_w = np.array([0.5 * (bins_w[i] + bins_w[i + 1]) for i in range(len(bins_w) - 1)])
+
+
+
+
 
 #pop_i, pcov_i = curve_fit(maxwellian, xdata=binscenters_i, ydata=data_entries_i, p0=[0.1,6,0.1])
 #popt_f, pcov_f = curve_fit(fit_function, xdata=binscenters_f, ydata=data_entries_f,maxfev=80000, p0=[0.1,1.2,0.01,1,6,1])
@@ -100,14 +115,14 @@ binscenters_f = np.array([0.5 * (bins_f[i] + bins_f[i + 1]) for i in range(len(b
 #zspace = maxwellian(xspace, *pop_i)
 #wspace = second_fit_function(xspace, *pop_w)
 
-x = np.linspace(0,15, 149)
-y = {label_i:data_entries_i,}# label_f:data_entries_f, }#'Wind 1 AU':wspace}
+x = np.linspace(0,14, bn_i)
+y = {'1.0 AU':data_entries_i, 'Wind @ 1.0 AU':data_entries_f, }#'1.0 AU':data_entries_w}
 
 
 y_label = 'Probability Density'
 x_label = r'$\alpha$-Proton Relative Temperature'
 
-color = ['blue']#,'black']#,'red']
-style = ['-']#,'--']#,'--']
+color = ['gray','black',]#'blue']
+style = ['-','--']#,'--']
 
-graph.graph(x, y, colours=color, style_line=style, title='', x_axis=x_label, y_axis=y_label, limits=True, x_lim=14, y_lim=0.5 )
+graph.graph(x, y, colours=color, style_line=style, title='', x_axis=x_label, y_axis=y_label, limits=True, x_lim=14, y_lim=1, line_width=2)
